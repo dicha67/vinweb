f390cafbae5bd16729993deaabbc299104e17655
Dirk <ddwacht@gmail.com>
2021-04-23 19:45:39 +0200

    about completed en hamburger menu

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,6 @@
 class MobileMenu {
   constructor() {
-    this.menuIcon = document.querySelector(".menu-icon");
+    this.menuIcon = document.querySelector(".header__hamburger-menu-icon");
     this.menuContent = document.querySelector(".header__menu-content");
     this.menuNav = document.querySelector(".header__menu-nav");
 
@@ -14,6 +14,7 @@ class MobileMenu {
   toggleTheMenu() {
     this.menuContent.classList.toggle("header__menu-content--is-visible");
     this.menuNav.classList.toggle("header__menu-nav--is-visible");
+    this.menuIcon.classList.toggle("header__hamburger-menu-icon--close-x");
   }
 }
 
