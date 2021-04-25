ee29d89d720d90b4211eb4d938fae86f3d0ce5f8
Dirk <ddwacht@gmail.com>
2021-04-25 17:38:09 +0200

    nog verbeteringen

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const currentTask = process.env.npm_lifecycle_event;
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fse = require("fs-extra");
 
 const postCSSPlugins = [
   require("postcss-import"),
@@ -9,30 +13,53 @@ const postCSSPlugins = [
   require("autoprefixer"),
 ];
 
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy images", function () {
+      fse.copySync("./app/assets/images", "./dist/assets/images");
+      fse.copySync("./app/assets/files", "./dist/assets/files");
+    });
+  }
+}
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: [
+    "css-loader?url=false",
+    {
+      loader: "postcss-loader",
+      options: { postcssOptions: { plugins: postCSSPlugins } },
+    },
+  ],
+};
+
+let pages = fse
+  .readdirSync("./app")
+  .filter(function (file) {
+    return file.endsWith(".html");
+  })
+  .map(function (page) {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`,
+    });
+  });
+
 let config = {
   entry: "./app/assets/scripts/App.js",
+  plugins: pages,
+
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader?url=false",
-          {
-            loader: "postcss-loader",
-            options: { postcssOptions: { plugins: postCSSPlugins } },
-          },
-        ],
-      },
-    ],
+    rules: [cssConfig],
   },
 };
 
 if (currentTask == "dev") {
-  (config.output = {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "app"),
-  }),
+  cssConfig.use.unshift("style-loader"),
+    (config.output = {
+      filename: "bundled.js",
+      path: path.resolve(__dirname, "app"),
+    }),
     (config.devServer = {
       before: function (app, server) {
         server._watch("./app/**/*.html");
@@ -46,11 +73,32 @@ if (currentTask == "dev") {
 }
 
 if (currentTask == "build") {
-  (config.output = {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "dist"),
-  }),
-    (config.mode = "production");
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+  });
+
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader),
+    (config.output = {
+      filename: "[name].[chunkhash].js",
+      chunkFilename: "[name].[chunkhash].js",
+      path: path.resolve(__dirname, "dist"),
+    }),
+    (config.mode = "production"),
+    (config.optimization = {
+      splitChunks: { chunks: "all" },
+    }),
+    config.plugins.push(
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }),
+      new RunAfterCompile()
+    );
 }
 
 module.exports = config;
